61c60ffeb2d62d30fc938888bc43433814637a6a
dejah marshall <2165302@talnet.nl>
2023-11-19 17:27:07 +0100

    Update db.php

--- db.php
+++ db.php
@@ -13,18 +13,36 @@ class Database {
         }
     }
 
-    public function addData($naam, $achternaam, $email) {
+    public function selectData($id = null) {
         try {
-            $sql = "contacts your_table_name (naam, achternaam, email) VALUES (:naam, :achternaam, :email)";
-            $stmt = $this->pdo->prepare($sql);
-            $stmt->bindParam(':naam', $naam);
-            $stmt->bindParam(':achternaam', $achternaam);
-            $stmt->bindParam(':email', $email);
+            if ($id !== null) {
+                $stmt = $this->pdo->prepare("SELECT * FROM contacts WHERE id = :id");
+                $stmt->bindParam(':id', $id);
+            } else {
+                $stmt = $this->pdo->query("SELECT * FROM contacts");
+            }
 
             $stmt->execute();
             $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+          
+            echo '<table border="1">';
+            echo '<tr><th>ID</th><th>Naam</th><th>Achternaam</th><th>Email</th><th>Edit</th><th>Delete</th></tr>';
+            foreach ($result as $row) {
+                echo '<tr>';
+                echo '<td>' . $row['id'] . '</td>';
+                echo '<td>' . $row['naam'] . '</td>';
+                echo '<td>' . $row['achternaam'] . '</td>';
+                echo '<td>' . $row['email'] . '</td>';
+                echo '<td><button>Edit</button></td>';
+                echo '<td><button>Delete</button></td>';
+                echo '</tr>';
+            }
+            echo '</table>';
+
+            return $result;
         } catch(PDOException $e) {
-            echo "Error adding data: " . $e->getMessage();
+            echo "Error selecting data: " . $e->getMessage();
         }
     }
 }
